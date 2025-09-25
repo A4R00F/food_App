1397da71ec0d51c9244ddfbf30eb8bc632692ea9
Ashraf Nagy <Ashraf@gmail.com>
2025-09-25 23:05:02 +0300

    Make The Bottom Button Always Appear

--- food_d/lib/pages/food_details.dart
+++ food_d/lib/pages/food_details.dart
@@ -15,86 +15,102 @@ class FoodDetails extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              FoodBannerDetails(foodindex: foodindex),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
+        body: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              food[foodindex].title,
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                            Text(
-                              "Buffalo Burger",
-                              style: Theme.of(context).textTheme.labelMedium!
-                                  .copyWith(color: Colors.grey),
-                            ),
-                          ],
-                        ),
-                        FoodItemCounter(),
-                      ],
-                    ),
-
-                    SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        PropertyItem(protitle: "Size", provalue: "Medium"),
-                        VerticalDivider(indent: 0, endIndent: 0),
-                        PropertyItem(
-                          provalue: "10-20 Min",
-                          protitle: "Cooking",
-                        ),
-                        VerticalDivider(indent: 0, endIndent: 0),
-                        PropertyItem(
-                          provalue: "150 k-cal",
-                          protitle: "Callories",
-                        ),
-                        SizedBox(height: 10),
-                      ],
-                    ),
-                    Text(
-                      'Lorem Ipsum Lorem Lorem Ipsum Lorem  Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem  ',
-                      style: Theme.of(
-                        context,
-                      ).textTheme.titleSmall!.copyWith(color: Colors.grey),
-                    ),
-                    SizedBox(height: 0.10),
-                    Row(
-                      children: [
-                        Text(
-                          "\$ ${food[foodindex].price}",
-                          style: Theme.of(context).textTheme.titleLarge!
-                              .copyWith(color: Theme.of(context).primaryColor),
-                        ),
-                        SizedBox(width: 10),
-                        Expanded(
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.white,
-                              backgroundColor: Colors.deepOrange,
-                            ),
-                            onPressed: () {},
-                            child: Text("Checkout"),
+                    FoodBannerDetails(foodindex: foodindex),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    food[foodindex].title,
+                                    style: Theme.of(
+                                      context,
+                                    ).textTheme.titleLarge,
+                                  ),
+                                  Text(
+                                    "Buffalo Burger",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .labelMedium!
+                                        .copyWith(color: Colors.grey),
+                                  ),
+                                ],
+                              ),
+                              FoodItemCounter(),
+                            ],
                           ),
-                        ),
-                      ],
+
+                          SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              PropertyItem(
+                                protitle: "Size",
+                                provalue: "Medium",
+                              ),
+                              VerticalDivider(indent: 0, endIndent: 0),
+                              PropertyItem(
+                                provalue: "10-20 Min",
+                                protitle: "Cooking",
+                              ),
+                              VerticalDivider(indent: 0, endIndent: 0),
+                              PropertyItem(
+                                provalue: "150 k-cal",
+                                protitle: "Callories",
+                              ),
+                              SizedBox(height: 10),
+                            ],
+                          ),
+                          Text(
+                            'Lorem Ipsum Lorem Lorem Ipsum Lorem  Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem  ',
+                            style: Theme.of(context).textTheme.titleSmall!
+                                .copyWith(color: Colors.grey),
+                          ),
+                          SizedBox(height: 0.10),
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Text(
+                    "\$ ${food[foodindex].price}",
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.deepOrange,
+                      ),
+                      onPressed: () {},
+                      child: Text("Checkout"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
