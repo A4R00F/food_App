b50a9a0e578abf3a12f947099c60f7880eeba27b
Ashraf Nagy <ygghhgggrdr123@gmail.com>
2025-09-28 06:39:52 +0300

    Add categroy item in home

---
+++ food_d/lib/models/category_item.dart
@@ -0,0 +1,47 @@
+import 'package:food_d/utilities/app_assets.dart';
+
+class CategoryItem {
+  final String name;
+  final String image;
+  final String id;
+
+  CategoryItem({required this.name, required this.image, required this.id});
+}
+
+List<CategoryItem> categories = [
+  CategoryItem(
+    name: 'Burger',
+    image: AppAssets.burgerIcon,
+    id: '1',
+  ),
+  CategoryItem(
+    name: 'Burger',
+    image: AppAssets.burgerIcon,
+    id: '1',
+  ),
+  CategoryItem(
+    name: 'Burger',
+    image: AppAssets.burgerIcon,
+    id: '1',
+  ),
+  CategoryItem(
+    name: 'Chicken',
+    image: AppAssets.chickenIcon,
+    id: '2',
+  ),
+  CategoryItem(
+    name: 'Chicken',
+    image: AppAssets.chickenIcon,
+    id: '2',
+  ),
+  CategoryItem(
+    name: 'Pizza',
+    image: AppAssets.pizzaIcon,
+    id: '3',
+  ),
+  CategoryItem(
+    name: 'Pizza',
+    image: AppAssets.pizzaIcon,
+    id: '3',
+  ),
+];
---
+++ food_d/lib/utilities/app_assets.dart
@@ -0,0 +1,10 @@
+class AppAssets {
+  // categories icons
+  static const String burgerIcon = 'assets/images/categories/burger-2.png';
+  static const String chickenIcon = 'assets/images/categories/chicken-leg.png';
+  static const String pizzaIcon = 'assets/images/categories/pizza_2.png';
+  //Home
+  static const String burger = 'assets/images/burger.jpg';
+  static const String chicken = 'assets/images/chicken.png';
+  static const String pizza = 'assets/images/pizza.png';
+}
